ffffacb772068c1ef4ef9a9a793846aec0736bbc
jbwexler <jbwexler@tutanota.com>
2024-05-14 16:07:33 -0700

    revert to re.split, use maxsplit=1

--- wbhiutils/parse_dicom_hdr.py
+++ wbhiutils/parse_dicom_hdr.py
@@ -3,7 +3,7 @@ import re
 # Parse a dicom header and return the pi-id and sub-id
 def parse_pi_sub(dcm_hdr, site): 
     if site == 'ucsb':
-        pi_id, sub_id = dcm_hdr["PatientName"].split('^')[:2]
+        pi_id, sub_id = re.split('[^0-9a-zA-Z]', dcm_hdr["PatientName"], maxsplit=1)
     elif site == 'uci':
         pi_id = re.split('[^0-9a-zA-Z]', dcm_hdr["PatientName"])[0]
         sub_id = re.split('[^0-9a-zA-Z]', dcm_hdr["PatientID"])[0]
@@ -11,6 +11,6 @@ def parse_pi_sub(dcm_hdr, site):
         pi_id = re.split(' ', dcm_hdr["StudyDescription"])[0]
         sub_id = dcm_hdr["PatientName"]
     else:
-        pi_id, sub_id = dcm_hdr["PatientName"].split('^')[:2]
+        pi_id, sub_id = re.split('[^0-9a-zA-Z]', dcm_hdr["PatientName"], maxsplit=1)
     
     return pi_id, sub_id
