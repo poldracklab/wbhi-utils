1c7897931a89a794c054b0d351d3c360620df140
jbwexler <jbwexler@tutanota.com>
2024-06-17 15:35:20 -0700

    add constants.py

---
+++ wbhiutils/constants.py
@@ -0,0 +1,20 @@
+DATE_FORMAT_FW = "%Y%m%d"
+DATETIME_FORMAT_FW = "%Y%m%d %H%M%S.%f"
+DATE_FORMAT_RC = "%Y-%m-%d"
+REDCAP_API_URL = "https://redcap.stanford.edu/api/"
+WBHI_ID_SUFFIX_LENGTH = 5 # An additional character corresponding to site will be prepended
+SITE_KEY = {
+        "ucsb": "A",
+        "ucb": "B",
+        "ucsf": "C",
+        "uci": "D",
+        "ucd": "E",
+        "stanford": "F"
+    }
+REDCAP_KEY = {
+    "am_pm": {
+        "am": "1",
+        "pm": "2"
+    }
+}
+SITE_LIST = ["ucsb", "uci", "ucb"]
