4f9cedeb0b5cd330901fe362674a767cecdfd518
jbwexler <jbwexler@tutanota.com>
2024-12-20 10:43:08 -0800

    handle ucr non-match

--- wbhiutils/parse_dicom_hdr.py
+++ wbhiutils/parse_dicom_hdr.py
@@ -16,11 +16,13 @@ def parse_pi(dcm_hdr: dict, site: str) -> str:
     elif site == 'ucr':
         if dcm_hdr["StudyDescription"].startswith("LC_Grant Final_Setup"):
             return "lc_grant"
-        return re.match(r"^(PSYCHOLOGY|BIO|BIOE)-([A-Za-z0-9]+)", dcm_hdr["StudyDescription"]).group(2)
+        match = re.match(r"^(PSYCHOLOGY|BIO|BIOE)-([A-Za-z0-9]+)", dcm_hdr["StudyDescription"])
+        if match:
+            return match.group(2)
+        else:
+            return None
     else:
         return re.split('[^0-9a-zA-Z]', dcm_hdr["PatientName"], maxsplit=1)[0]
-    
-    return pi_id, sub_id
 
 def parse_sub(dcm_hdr: dict, site: str) -> str:
     """Parse a dicom header and return the Sub-ID"""
