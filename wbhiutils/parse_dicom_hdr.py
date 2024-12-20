2c712c173dbc49891a4e7ff8e960816c00d0d93a
jbwexler <jbwexler@stanford.edu>
2024-12-19 16:31:03 -0800

    Add UCR to parse_dicom_hdr.py

--- wbhiutils/parse_dicom_hdr.py
+++ wbhiutils/parse_dicom_hdr.py
@@ -13,6 +13,10 @@ def parse_pi(dcm_hdr: dict, site: str) -> str:
         return dcm_hdr["ReferringPhysicianName"]
     elif site == 'ucsf':
         return re.split('[^0-9a-zA-Z]', dcm_hdr["StudyDescription"], maxsplit=1)[1]
+    elif site == 'ucr':
+        if dcm_hdr["StudyDescription"].startswith("LC_Grant Final_Setup"):
+            return "lc_grant"
+        return re.match(r"^(PSYCHOLOGY|BIO|BIOE)-([A-Za-z0-9]+)", dcm_hdr["StudyDescription"]).group(2)
     else:
         return re.split('[^0-9a-zA-Z]', dcm_hdr["PatientName"], maxsplit=1)[0]
     
@@ -30,5 +34,7 @@ def parse_sub(dcm_hdr: dict, site: str) -> str:
         return dcm_hdr["PatientID"]
     elif site == 'ucsf':
         return dcm_hdr["PatientID"]
+    elif site == 'ucr':
+        return dcm_hdr["PatientName"]
     else:
         return re.split('[^0-9a-zA-Z]', dcm_hdr["PatientName"], maxsplit=1)[1]
