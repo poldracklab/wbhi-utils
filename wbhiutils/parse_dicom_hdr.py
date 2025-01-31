e2fbd29e5498f5e789c55629ba024fa1c28cf45a
Christopher J. Markiewicz <markiewicz@stanford.edu>
2025-01-30 21:25:55 -0800

    feat: Add py.typed

--- wbhiutils/parse_dicom_hdr.py
+++ wbhiutils/parse_dicom_hdr.py
@@ -1,7 +1,7 @@
 import re
 
 # Parse a dicom header and return the pi-id and sub-id
-def parse_pi(dcm_hdr: dict, site: str) -> str:
+def parse_pi(dcm_hdr: dict[str, str], site: str) -> str:
     """Parse a dicom header and return the PI-ID"""
     if site == 'ucsb':
         return re.split('[^0-9a-zA-Z]', dcm_hdr["PatientName"], maxsplit=1)[0]
@@ -19,12 +19,11 @@ def parse_pi(dcm_hdr: dict, site: str) -> str:
         match = re.match(r"^(PSYCHOLOGY|BIO|BIOE)-([A-Za-z0-9]+)", dcm_hdr["StudyDescription"])
         if match:
             return match.group(2)
-        else:
-            return None
+        raise ValueError(f"Could not parse PI from StudyDescription: {dcm_hdr['StudyDescription']}")
     else:
         return re.split('[^0-9a-zA-Z]', dcm_hdr["PatientName"], maxsplit=1)[0]
 
-def parse_sub(dcm_hdr: dict, site: str) -> str:
+def parse_sub(dcm_hdr: dict[str, str], site: str) -> str:
     """Parse a dicom header and return the Sub-ID"""
     if site == 'ucsb':
         return re.split('[^0-9a-zA-Z]', dcm_hdr["PatientName"], maxsplit=1)[1]
